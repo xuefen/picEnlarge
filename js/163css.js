ff856ac9d776e7cbbf99c1aaa9afff56193ed6da
xuefen <xuefennh@yahoo.cn>
2013-04-13 20:08:13 +0800

    创建
    创建图片放大

---
+++ js/163css.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+		
+$('.photo_slider').each(function(){
+	
+	var $this = $(this).addClass('photo-area');
+	var $img = $this.find('img');
+	var $info = $this.find('.info_area');
+		
+	var opts = {};
+	
+	$img.ready(function(){
+		opts.imgw = $img.width();
+		opts.imgh = $img.height();
+	});
+	
+	opts.orgw = $this.width();
+	opts.orgh =  $this.height();
+	
+	$img.css ({
+		marginLeft: "-150px",
+		marginTop: "-150px"
+	});
+	
+	var $wrap = $('<div class="photo_slider_img">').append($img).prependTo($this);
+
+	var $open = $img;
+	
+	var $close = $('<a class="close">还原</a>').appendTo($info);
+	
+	opts.wrapw = $wrap.width();
+	opts.wraph = $wrap.height();
+	
+	$open.click(function(){
+		$this.animate({ 
+			width: opts.imgw,
+			height: opts.imgh,
+			borderWidth: "1"
+			
+		}, 600 );
+		$wrap.animate({ 
+		    width: opts.imgw,
+			height: opts.imgh
+		}, 600 );
+
+		$(".info_area",$this).fadeIn();
+		
+		$img.animate({
+			marginTop: "0px",
+			marginLeft: "0px"
+		}, 600 );
+		
+		return false;
+	});
+	
+	$close.click(function(){
+		$this.animate({ 
+			width: opts.orgw,
+			height: opts.orgh,
+	        borderWidth: "1"
+	      }, 600 );
+		
+		$open.fadeIn();
+		
+		$wrap.animate({ 
+	        width: opts.wrapw,
+			height: opts.wraph
+		      }, 600 );
+		
+			$img.animate({
+				marginTop: "-150px",
+				marginLeft: "-150px"
+			}, 600 );
+
+		$(".info_area",$this).fadeOut();
+		return false;
+	});
+	
+});
+
+});
